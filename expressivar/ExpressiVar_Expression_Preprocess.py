49174464d02562b6461b1f3dd8a6cb933340bdca
Kundai Andrew Midzi <kundai.andrew.midzi@gmail.com>
2018-08-08 16:43:47 -0400

    Re-factors preprocessing script.

--- expressivar/ExpressiVar_Expression_Preprocess.py
+++ expressivar/ExpressiVar_Expression_Preprocess.py
@@ -1,42 +1,56 @@
-import sys
+DEFAULT_ENSEMBLE_DB = 'Ensembl_GeneName.txt'
 
-input_filename = str(sys.argv[1])
 
-file_in_Ensembl_Genename = open('Ensembl_GeneName.txt', 'r')
+def preprocess_expressions(infile, outfile=None, ensemble_db=DEFAULT_ENSEMBLE_DB):
+    if outfile is None:
+        outfile = infile + '.expression.txt'
 
-file_in = open(input_filename, 'r')
-file_out = open(input_filename + '.expression.txt', 'w')
+    with open(input_filename, 'r') as file_in, open(outfile, 'w') as file_out, open(
+        ensemble_db
+    ) as file_in_Ensembl_Genename:
 
-Ensembl_Gene = dict()
-Ensembl_set = set()
-for eachline in file_in_Ensembl_Genename:
-	component = eachline.strip().split('\t')
-	ensembl = component[0]
-	gene = component[1]
-	Ensembl_Gene[ensembl] = gene
-	Ensembl_set.add(ensembl)
+        Ensembl_Gene = dict()
+        Ensembl_set = set()
+        for eachline in file_in_Ensembl_Genename:
+            component = eachline.strip().split('\t')
+            ensembl = component[0]
+            gene = component[1]
+            Ensembl_Gene[ensembl] = gene
+            Ensembl_set.add(ensembl)
 
-file_in.readline()
-file_in.readline()
-gene_expression_dict_list = dict()
-gene_set = set()
-for eachline in file_in:
-	component = eachline.strip().split('\t')
-	ID = component[0].split('.')[0]
-	reads = float(component[6])
+        file_in.readline()
+        file_in.readline()
+        gene_expression_dict_list = dict()
+        gene_set = set()
+        for eachline in file_in:
+            component = eachline.strip().split('\t')
+            ID = component[0].split('.')[0]
+            reads = float(component[6])
 
-	if ID in Ensembl_set:
-		gene = Ensembl_Gene[ID]
-		gene_set.add(gene)
-		if gene not in gene_expression_dict_list.keys():
-			gene_expression_dict_list[gene] = []
-			gene_expression_dict_list[gene].append(reads)
-		else:
-			gene_expression_dict_list[gene].append(reads)
+            if ID in Ensembl_set:
+                gene = Ensembl_Gene[ID]
+                gene_set.add(gene)
+                if gene not in gene_expression_dict_list.keys():
+                    gene_expression_dict_list[gene] = []
+                    gene_expression_dict_list[gene].append(reads)
+                else:
+                    gene_expression_dict_list[gene].append(reads)
 
-gene_list = list(gene_set)
-gene_list.sort()
-for eachgene in gene_list:
-	gene_expression_list = gene_expression_dict_list[eachgene]
-	gene_expression = sum(gene_expression_list) / len(gene_expression_list)
-	file_out.write(eachgene + '\t' + str(round(gene_expression, 3)) + '\n')
+        gene_list = list(gene_set)
+        gene_list.sort()
+        for eachgene in gene_list:
+            gene_expression_list = gene_expression_dict_list[eachgene]
+            expr_sum = sum(gene_expression_list)
+            gene_expression = expr_sum / len(gene_expression_list)
+            file_out.write(eachgene + '\t' + str(round(gene_expression, 3)) + '\n')
+
+
+if __name__ == '__main__':
+    import sys
+
+    try:
+        input_filename = str(sys.argv[1])
+    except IndexError:
+        sys.exit('Supply input-file, output-file')
+
+    preprocess_expressions(input_filename)
